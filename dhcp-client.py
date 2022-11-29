03eafa3c1703b19f1e5a48eca7f86792a6608e7c
princesssuna <1009suna@gmail.com>
2022-11-29 14:47:19 +0900

    feat: dhcp-client.py change field

--- dhcp-client.py
+++ dhcp-client.py
@@ -34,24 +34,26 @@ class DHCP_client(object):
         HTYPE = bytes([0x01])
         HLEN = bytes([0x06])
         HOPS = bytes([0x00])
-        XID = bytes([0x39, 0x03, 0xF3, 0x26])
+
+        #
+        XID = bytes([0x39, 0x03, 0xF3, 0x26]) # Same Transaction ID
         SECS = bytes([0x00, 0x00])
-        FLAGS = bytes([0x00, 0x00])
+        FLAGS = bytes([0x80, 0x00]) # Flags (0x8000: Broadcast Flag)
         CIADDR = bytes([0x00, 0x00, 0x00, 0x00])
         YIADDR = bytes([0x00, 0x00, 0x00, 0x00])
         SIADDR = bytes([0x00, 0x00, 0x00, 0x00])
         GIADDR = bytes([0x00, 0x00, 0x00, 0x00])
-        CHADDR1 = bytes([0x00, 0x05, 0x3C, 0x04]) 
-        CHADDR2 = bytes([0x8D, 0x59, 0x00, 0x00]) 
+
+        CHADDR1 = bytes([0xb6, 0xaa, 0x0C, 0x80]) # Client MAC Address
+        CHADDR2 = bytes([0xaf, 0x21, 0x00, 0x00]) 
         CHADDR3 = bytes([0x00, 0x00, 0x00, 0x00]) 
         CHADDR4 = bytes([0x00, 0x00, 0x00, 0x00]) 
         CHADDR5 = bytes(192)
         Magiccookie = bytes([0x63, 0x82, 0x53, 0x63])
-        DHCPOptions1 = bytes([53 , 1 , 1])
-        DHCPOptions2 = bytes([50 , 4 , 0xC0, 0xA8, 0x01, 0x64])
+        DHCPOptions1 = bytes([53 , 1 , 1]) # DHCP Message Type (Discover)
+        DHCPOptions2 = bytes([50 , 4 , 0xC0, 0xA8, 0x00, 0x09]) # Requested IP Address: 192.168.0.9
 
-
-        package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR +YIADDR + SIADDR + GIADDR + CHADDR1 + CHADDR2 + CHADDR3 + CHADDR4 + CHADDR5 + Magiccookie + DHCPOptions1 + DHCPOptions2
+        package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR + YIADDR + SIADDR + GIADDR + CHADDR1 + CHADDR2 + CHADDR3 + CHADDR4 + CHADDR5 + Magiccookie + DHCPOptions1 + DHCPOptions2
 
         return package
 
@@ -60,24 +62,27 @@ class DHCP_client(object):
         HTYPE = bytes([0x01])
         HLEN = bytes([0x06])
         HOPS = bytes([0x00])
-        XID = bytes([0x39, 0x03, 0xF3, 0x26])
+
+        #
+        XID = bytes([0x39, 0x03, 0xF3, 0x26]) # Same Transaction ID
         SECS = bytes([0x00, 0x00])
-        FLAGS = bytes([0x00, 0x00])
+        FLAGS = bytes([0x80, 0x00]) # Flags (0x8000: Broadcast Flag)
         CIADDR = bytes([0x00, 0x00, 0x00, 0x00])
         YIADDR = bytes([0x00, 0x00, 0x00, 0x00])
         SIADDR = bytes([0x00, 0x00, 0x00, 0x00])
         GIADDR = bytes([0x00, 0x00, 0x00, 0x00])
-        CHADDR1 = bytes([0x00, 0x0C, 0x29, 0xDD]) 
-        CHADDR2 = bytes([0x5C, 0xA7, 0x00, 0x00])
+        
+        CHADDR1 = bytes([0xb6, 0xaa, 0x0C, 0x80]) # Client MAC Address
+        CHADDR2 = bytes([0xaf, 0x21, 0x00, 0x00])
         CHADDR3 = bytes([0x00, 0x00, 0x00, 0x00]) 
         CHADDR4 = bytes([0x00, 0x00, 0x00, 0x00]) 
         CHADDR5 = bytes(192)
         Magiccookie = bytes([0x63, 0x82, 0x53, 0x63])
-        DHCPOptions1 = bytes([53 , 1 , 3])
-        DHCPOptions2 = bytes([50 , 4 , 0xC0, 0xA8, 0x01, 0x64])
-        DHCPOptions3 = bytes([54 , 4 , 0xC0, 0xA8, 0x01, 0x01])
+        DHCPOptions1 = bytes([53 , 1 , 3]) # DHCP Message Type (Request)
+        DHCPOptions2 = bytes([50 , 4 , 0xC0, 0xA8, 0x00, 0x09]) # Requested IP Address: 192.168.0.9
+        DHCPOptions3 = bytes([54 , 4 , 0xC0, 0xA8, 0x00, 0x01]) # DHCP Server Identifier: 192.168.0.1
 	
-        package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR +YIADDR + SIADDR + GIADDR + CHADDR1 + CHADDR2 + CHADDR3 + CHADDR4 + CHADDR5 + Magiccookie + DHCPOptions1 + DHCPOptions2 +  DHCPOptions3
+        package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR + YIADDR + SIADDR + GIADDR + CHADDR1 + CHADDR2 + CHADDR3 + CHADDR4 + CHADDR5 + Magiccookie + DHCPOptions1 + DHCPOptions2 +  DHCPOptions3
 
         return package
 
